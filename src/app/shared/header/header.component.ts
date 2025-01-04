fd4c0760b7039fcc1df8dacf0fe59db2b3a69a46
Sven Plankenbichler <128075628+Trouble2701@users.noreply.github.com>
2025-01-04 22:13:40 +0100

    header menu added

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { LoginComponent } from '../../component/login/login.component';
 import { GamedaycounterComponent } from '../gamedaycounter/gamedaycounter.component';
+import { MenuComponent } from '../menu/menu.component';
 
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [LoginComponent, GamedaycounterComponent],
+  imports: [LoginComponent, GamedaycounterComponent, MenuComponent],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
