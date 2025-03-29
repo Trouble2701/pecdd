fb1b204620cb8ca73bf07d6103cabd6adf8a943d
Trouble2701 <troublemaker1988@online.de>
2025-03-29 16:39:36 +0100

    delete php datas and ods

--- php/api/user.php
+++
@@ -1,9 +0,0 @@
-<?php
-require('db.php');
-
-if($method === "GET"){
-    $result = $conn->query("SELECT * FROM users");
-    $users = $result->fetch_all(MYSQLI_ASSOC);
-    echo json_encode($users);
-}
-?>
