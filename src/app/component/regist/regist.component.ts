e20e9a94c2670e9f3ac31112a3a577f6135ac743
Trouble2701 <troublemaker1988@online.de>
2025-04-04 16:48:40 +0200

    regist added birthday and gender

--- src/app/component/regist/regist.component.ts
+++ src/app/component/regist/regist.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
+import { CommonModule, NgIf, NgFor } from '@angular/common';
 import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-regist',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, NgIf, NgFor],
   templateUrl: './regist.component.html',
   styleUrl: './regist.component.scss'
 })
@@ -14,5 +15,27 @@ export class RegistComponent {
 
   sendCode(){
     this.router.navigate(['/registcode']);
+  }
+
+  dropdownOpen = false;
+  selectedOption = 'Bitte Wählen';
+  options = [
+    { label: 'Männlich', value: 'm' },
+    { label: 'Weiblich', value: 'w' }
+  ];
+
+  toggleDropdown(event: Event) {
+    event.stopPropagation(); // Verhindert das Schließen direkt nach dem Öffnen
+    this.dropdownOpen = !this.dropdownOpen;
+  }
+
+  selectOption(option: { label: string; value: string }) {
+    this.selectedOption = option.label;
+    this.dropdownOpen = false;
+  }
+
+  @HostListener('document:click', ['$event'])
+  closeDropdown(event?: Event) {
+    this.dropdownOpen = false;
   }
 }
