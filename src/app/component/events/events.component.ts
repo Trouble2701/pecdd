7ba855782739d3ba322b21d14030068a03bba28b
Trouble2701 <troublemaker1988@online.de>
2025-08-01 20:27:30 +0200

    events designed

--- src/app/component/events/events.component.ts
+++ src/app/component/events/events.component.ts
@@ -8,4 +8,21 @@ import { Component } from '@angular/core';
 })
 export class EventsComponent {
 
+openAdress(area: any) {
+    if (area && area.adress && area.number && area.zip_code && area.location) {
+      // Adresse zusammensetzen
+      let adresse = area.adress + ' ' + area.number + ', ' + area.zip_code + ' ' + area.location;
+      
+      // Adresse URL-encodieren
+      const encodedAdresse = encodeURIComponent(adresse);
+      
+      // Google Maps URL erstellen
+      const googleMapsUrl = `https://www.google.com/maps?q=${encodedAdresse}`;
+      
+      // Google Maps in neuem Tab öffnen
+      console.log(window.open(googleMapsUrl, "_blank"));
+    } else {
+      console.error('Die Adresse ist unvollständig:', area);
+    }
+  }
 }
